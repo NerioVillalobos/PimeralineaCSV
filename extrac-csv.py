1896721cf1ae00f03715299a7202095f27fbbc01
Nerio Villalobos <nervill@gmail.com>
2020-12-01 03:03:16 -0300

    intento con numpy

--- extrac-csv.py
+++ extrac-csv.py
@@ -1,14 +1,16 @@
 import csv
 from collections import defaultdict
+import numpy
 
-columns = defaultdict(list)
-
+#columns = defaultdict(list)
+columns = numpy.array([1,2,3])
 
 with open('Account.csv') as f:
     reader = csv.DictReader(f) # read rows into a dictionary format
     for row in reader: # read a row as {column1: value1, column2: value2,...}
         for (k,v) in row.items(): # go over each column name and value 
-            columns[k].append(v) # append the value into the appropriate list
+            #columns[k].append(v) # append the value into the appropriate list
+            newArray = numpy.insert(columns, [1, columns[k].append(v)])
                                  # based on column name k
 
 
@@ -19,5 +21,5 @@ with open('Account.csv') as f:
 #            columns[k].extend(v) # append the value into the appropriate list
                                  # based on column name k
 
-formatted_string = "\n".join(columns['name'])
-print(formatted_string)
+#formatted_string = "\n".join(columns['name'])
+print(newArray)
